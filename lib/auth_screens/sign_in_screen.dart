ea37a835a6ba30db7a2e6b367ef63fe0c1c1e6d9
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-09-07 23:50:10 +0500

    Changes made

--- lib/auth_screens/sign_in_screen.dart
+++ lib/auth_screens/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:to_do_list/auth_functions/auth_functions.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -21,22 +22,20 @@ class _SignInScreenState extends State<SignInScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        padding: const EdgeInsets.only(left: 30, right: 30),
+        padding: EdgeInsets.only(left: width * 0.1, right: width * 0.1),
         height: height,
         width: width,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/logo.png',
-              fit: BoxFit.cover,
-              height: height - 400,
+            Container(
+              margin: EdgeInsets.only(top: 150.sp),
+              child: Image.asset('assets/images/logo.png',
+                  fit: BoxFit.cover, height: 130.sp),
             ),
-            // Container(
-            //   decoration: const BoxDecoration(
-            //       image: DecorationImage(
-            //           fit: BoxFit.cover,
-            //           image: AssetImage('assets/images/logo.png'))),
-            // ),
+            SizedBox(
+              height: 150.sp,
+            ),
             Form(
                 child: Column(
               children: [
@@ -50,12 +49,19 @@ class _SignInScreenState extends State<SignInScreen> {
                     return null;
                   },
                   decoration: const InputDecoration(
+                      hintStyle:
+                          TextStyle(color: Color.fromRGBO(39, 39, 39, 0.5)),
                       hintText: 'Email',
                       border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(39, 39, 39, 0.5)),
                           borderRadius: BorderRadius.all(Radius.circular(20)))),
                   onSaved: (newValue) {
                     email = newValue!;
                   },
+                ),
+                SizedBox(
+                  height: 10.sp,
                 ),
                 TextFormField(
                   obscureText: true,
@@ -67,17 +73,67 @@ class _SignInScreenState extends State<SignInScreen> {
                     }
                     return null;
                   },
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.remove_red_eye_outlined)),
+                      hintStyle: const TextStyle(
+                          color: Color.fromRGBO(39, 39, 39, 0.5)),
                       hintText: 'Password',
-                      border: OutlineInputBorder(
+                      border: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(39, 39, 39, 0.5)),
                           borderRadius: BorderRadius.all(Radius.circular(20)))),
                   onSaved: (newValue) {
                     password = newValue!;
                   },
                 ),
               ],
-            ))
+            )),
+            SizedBox(
+              height: 35.sp,
+            ),
+            CustomButton(
+                () {}, const Color.fromRGBO(247, 158, 137, 1), 'SIGN IN'),
+            SizedBox(
+              width: 210.sp,
+              child: Row(
+                children: [
+                  const Text('Don\'t have an account?'),
+                  TextButton(onPressed: () {}, child: const Text('Sign up'))
+                ],
+              ),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(this.function, this.backGroundColor, this.text,
+      {super.key});
+
+  final Function function;
+  final Color backGroundColor;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => function(),
+      child: Container(
+        height: 48.sp,
+        width: 327.sp,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: backGroundColor,
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.white),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'auth_screens/sign_in_screen.dart';
 // ignore: depend_on_referenced_packages
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,17 +12,21 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SignInScreen(),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (BuildContext context, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const SignInScreen(),
+          );
+        });
   }
 }
